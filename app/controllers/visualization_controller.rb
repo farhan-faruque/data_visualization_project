006f784ba3a7537888e9c3f416a020d110ef7bf5
Farhan Faruque <russel.cse05@gmail.com>
2015-08-20 16:35:28 +0600

    added

--- app/controllers/visualization_controller.rb
+++ app/controllers/visualization_controller.rb
@@ -5,6 +5,8 @@ class VisualizationController < ApplicationController
 
   def projects
     client = Mongo::Client.new('mongodb://farhan:123456@ds031903.mongolab.com:31903/donorschoose')
+    #client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'donorschoose')
+    #client = Mongo::Client.new('mongodb://127.0.0.1:27017/donorschoose')
     fields = {'school_state' => true , 'resource_type' => true, 'poverty_level' => true, 'date_posted' => true, 'total_donations' => true, '_id' => false }
     @projects = client[:projects].find.projection(fields)
 
